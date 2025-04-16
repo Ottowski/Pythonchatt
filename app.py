efabf8b868096e10a6a89f937d9eb00adbe5c302
Ottowski <otto.arvidsson94@gmail.com>
2025-04-16 18:22:14 +0200

    GUI now working

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pymongo
 #Connect to MongoDB
 client  = pymongo.MongoClient("mongodb://localhost:27017")
 db= client["Pythonchat"]
-meesasges_col = db["messages"]
+messages_col = db["messages"]
 
 # GUI
 root = tk.Tk()
@@ -14,6 +14,13 @@ root.geometry("300x300")
 entry = tk.Entry(root, width=40)
 entry.pack(pady=5)
 
-send_button = tk.Button(root, text="Send")
+def send_message():
+    if entry.get():
+        messages_col.insert_one({"text": entry.get()})
+        entry.delete(0, tk.END)
+
+send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
+
+
 root.mainloop()
