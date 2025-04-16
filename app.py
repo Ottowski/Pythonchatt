d7f68f17eac2d60c5ac0a2731b0097d97212ba04
Ottowski <otto.arvidsson94@gmail.com>
2025-04-16 18:12:53 +0200

    added GUI

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 import tkinter as tk
 import pymongo
+
+#Connect to MongoDB
+client  = pymongo.MongoClient("mongodb://localhost:27017")
+db= client["Pythonchat"]
+meesasges_col = db["messages"]
+
+# GUI
+root = tk.Tk()
+root.title("Pythonchat")
+root.geometry("300x300")
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=5)
+
+send_button = tk.Button(root, text="Send")
+send_button.pack(pady=5)
+root.mainloop()
